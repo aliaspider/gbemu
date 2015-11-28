4c3e093a74fe20644ad400c9252bb213df33e94d
aliaspider <aliaspider@gmail.com>
2015-11-28 22:24:49 +0100

    disassembler cont.

--- cpudisasm.c
+++ cpudisasm.c
@@ -11,6 +11,7 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
    static const char* cond_names[] = {"NZ", "Z", "NC", "C"};
 
    char immediate8[3];
+   char sp_plus_immediate8[6];
    char immediate16[5];
    char immediate8_addr[5];
    char immediate16_addr[7];
@@ -70,6 +71,7 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
 
 
    snprintf(immediate8, sizeof(immediate8), "%02X", GB.MEMORY[(CPU->PC + 1) & 0xFFFF]);
+   snprintf(sp_plus_immediate8, sizeof(sp_plus_immediate8), "SP+%02X", GB.MEMORY[(CPU->PC + 1) & 0xFFFF]);
    snprintf(immediate16, sizeof(immediate16), "%02X%02X", GB.MEMORY[(CPU->PC + 2) & 0xFFFF],
             GB.MEMORY[(CPU->PC + 1) & 0xFFFF]);
    snprintf(immediate8_addr, sizeof(immediate8_addr), "(%02X)", GB.MEMORY[(CPU->PC + 1) & 0xFFFF]);
@@ -91,8 +93,59 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
       op.operand0 = immediate8;
       op.size++;
       break;
-   case 0x13:
-      op.label = "DI";
+   case 0x07:
+      op.label = "RLCA";
+      op.zero = "0";
+      op.negative = "0";
+      op.halfcarry = "0";
+      op.carry = "C";
+      break;
+   case 0x0F:
+      op.label = "RRCA";
+      op.zero = "0";
+      op.negative = "0";
+      op.halfcarry = "0";
+      op.carry = "C";
+      break;
+   case 0x17:
+      op.label = "RLA";
+      op.zero = "0";
+      op.negative = "0";
+      op.halfcarry = "0";
+      op.carry = "C";
+      break;
+   case 0x1F:
+      op.label = "RRA";
+      op.zero = "0";
+      op.negative = "0";
+      op.halfcarry = "0";
+      op.carry = "C";
+      break;
+   case 0x27:
+      op.label = "DAA";
+      op.zero = "Z";
+      op.halfcarry = "0";
+      op.carry = "C";
+      break;
+   case 0x2F:
+      op.label = "CPL";
+      op.negative = "1";
+      op.halfcarry = "1";
+      break;
+   case 0x37:
+      op.label = "SCF";
+      op.negative = "0";
+      op.halfcarry = "0";
+      op.carry = "1";
+      break;
+   case 0x3F:
+      op.label = "CCF";
+      op.negative = "0";
+      op.halfcarry = "0";
+      op.carry = "C";
+      break;
+   case 0x76:
+      op.label = "HALT";
       break;
    case 0xC3:
       op.label = "JP";
@@ -100,12 +153,18 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
       op.size += 2;
       op.cycles += 3;
       break;
+   case 0xF3:
+      op.label = "DI";
+      break;
+   case 0xFB:
+      op.label = "EI";
+      break;
    case 0xCB:
       op.val = GB.MEMORY[(CPU->PC + 1) & 0xFFFF];
       op.size++;
       switch (op.val)
       {
-
+         op.label = "CB op";
       }
       break;
    case 0xCD:
@@ -128,7 +187,40 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
       op.size++;
       op.cycles = 3;
       break;
-
+   case 0xE8:
+      op.label = "ADD";
+      op.operand0 = "SP";
+      op.operand1 = immediate8;
+      op.size++;
+      op.cycles = 4;
+      op.zero = "0";
+      op.negative = "0";
+      op.halfcarry = "H";
+      op.carry = "C";
+      break;
+   case 0xF8:
+      op.label = "LD";
+      op.operand0 = "HL";
+      op.operand1 = sp_plus_immediate8;
+      op.size++;
+      op.cycles = 3;
+      op.zero = "0";
+      op.negative = "0";
+      op.halfcarry = "H";
+      op.carry = "C";
+      break;
+   case 0xD3:
+   case 0xDB:
+   case 0xDD:
+   case 0xE3:
+   case 0xE4:
+   case 0xEB:
+   case 0xEC:
+   case 0xED:
+   case 0xF4:
+   case 0xFC:
+   case 0xFD:
+      op.label = "INVALID";
    default:
       switch (op.m11000000)
       {
@@ -144,7 +236,7 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
             else
             {
                op.operand0 = cond_names[op.r3];
-               op.cycles2 = 8;
+               op.cycles2 = 2;
                op.operand1 = immediate8;
             }
             break;
@@ -183,12 +275,39 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
             }
             break;
          case 0b011:
+            op.operand0 = reg16_names[op.r2];
+            op.cycles++;
+            if (op.m00001000 == 0)
+               op.label = "INC";
+            else
+               op.label = "DEC";
             break;
          case 0b100:
+            op.label = "INC";
+            op.operand0 = reg_names[op.r0];
+            op.zero = "Z";
+            op.negative = "0";
+            op.halfcarry = "H";
+            if (op.r0 == 0b110)
+               op.cycles += 2;
             break;
          case 0b101:
+            op.label = "DEC";
+            op.operand0 = reg_names[op.r0];
+            op.zero = "Z";
+            op.negative = "1";
+            op.halfcarry = "H";
+            if (op.r0 == 0b110)
+               op.cycles += 2;
             break;
          case 0b110:
+            op.label = "LD";
+            op.operand0 = reg_names[op.r0];
+            op.operand1 = immediate8;
+            op.size++;
+            op.cycles ++;
+            if (op.r0 == 0b110)
+               op.cycles ++;
             break;
          case 0b111:
             break;
@@ -202,19 +321,136 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
          op.operand1 = reg_names[op.r1];
          break;
       case 0b10:
+         op.zero = "Z";
+         op.operand0 = "A";
+         op.operand1 = reg_names[op.r1];
+         if (op.r0 == 0b110)
+            op.cycles++;
+         switch (op.r0)
+         {
+         case 0b000:
+            op.label = "ADD";
+            op.negative = "0";
+            op.halfcarry = "H";
+            op.carry = "C";
+            break;
+         case 0b001:
+            op.label = "ADC";
+            op.negative = "0";
+            op.halfcarry = "H";
+            op.carry = "C";
+            break;
+         case 0b010:
+            op.label = "SUB";
+            op.negative = "1";
+            op.halfcarry = "H";
+            op.carry = "C";
+            break;
+         case 0b011:
+            op.label = "SBC";
+            op.negative = "1";
+            op.halfcarry = "H";
+            op.carry = "C";
+            break;
+         case 0b100:
+            op.label = "AND";
+            op.negative = "0";
+            op.halfcarry = "1";
+            op.carry = "0";
+            break;
+         case 0b101:
+            op.label = "XOR";
+            op.negative = "0";
+            op.halfcarry = "0";
+            op.carry = "0";
+            break;
+         case 0b110:
+            op.label = "OR";
+            op.negative = "0";
+            op.halfcarry = "0";
+            op.carry = "0";
+            break;
+         case 0b111:
+            op.label = "CP";
+            op.negative = "1";
+            op.halfcarry = "H";
+            op.carry = "C";
+            break;
+         }
+
          break;
       case 0b11:
          switch (op.m00000111)
          {
          case 0b000:
+            op.label = "RET";
+            op.cycles = 5;
+            op.cycles2 = 2;
+            op.operand0 = cond_names[op.r3];
             break;
          case 0b001:
+            op.label = "POP";
+            op.cycles = 3;
+            if (op.r2 == 0b11)
+            {
+               op.operand0 = "AF";
+               op.zero = "Z";
+               op.negative = "N";
+               op.halfcarry = "H";
+               op.carry = "C";
+            }
+            else
+               op.operand0 = reg16_names[op.r2];
             break;
          case 0b010:
+            if (op.m00100000 == 0)
+            {
+               op.label = "JP";
+               op.operand0 = cond_names[op.r3];
+               op.operand1 = immediate16;
+               op.size += 2;
+               op.cycles = 4;
+               op.cycles2 = 3;
+            }
+            else
+            {
+               op.label = "LD";
+               switch (op.r3)
+               {
+               case 0b00:
+                  op.operand0 = "(FF00 + C)";
+                  op.operand1 = "A";
+                  op.size++;
+                  break;
+               case 0b10:
+                  op.operand0 = "A";
+                  op.operand1 = "(FF00 + C)";
+                  op.size++;
+                  break;
+               case 0b01:
+                  op.operand0 = immediate16_addr;
+                  op.operand1 = "A";
+                  op.size += 2;
+                  op.cycles = 4;
+                  break;
+               case 0b11:
+                  op.operand0 = "A";
+                  op.operand1 = immediate16_addr;
+                  op.size += 2;
+                  op.cycles = 4;
+                  break;
+               }
+            }
             break;
-         case 0b011:
-            break;
+         // case 0b011:
+         //    break;
          case 0b100:
+            op.label = "CALL";
+            op.operand0 = cond_names[op.r3];
+            op.operand1 = immediate16;
+            op.size += 2;
+            op.cycles = 6;
+            op.cycles2 = 3;
             break;
          case 0b101:
             break;
@@ -295,14 +531,14 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
 
    if (op.label)
    {
-      printf(" +%i", op.cycles);
+      printf(" %i", op.cycles);
       if (op.cycles2)
-         printf("/%i", op.cycles2);
+         printf("|%i", op.cycles2);
       else
          printf("  ");
 
-      printf(" (%i) (%s %s %s %s)  %s",
-             op.size, op.zero, op.negative, op.halfcarry, op.carry,
+      printf(" (%s %s %s %s)  %s",
+             op.zero, op.negative, op.halfcarry, op.carry,
              op.label);
       if (op.operand0)
          printf(" %s", op.operand0);
@@ -310,7 +546,7 @@ int gbemu_disasm_current(gbemu_cpu_t* CPU)
          printf(", %s", op.operand1);
    }
    else
-      printf(" +?   (?) (? ? ? ?)  ????");
+      printf(" ?   (? ? ? ?)  ????");
 
 
 
